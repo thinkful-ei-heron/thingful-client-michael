c833a03be15bc2ad570e3fb6ef8dae26554dbbde
Michael Kirsch <michaelskirsch@gmail.com>
2019-11-13 15:18:53 -0700

    finished assignment

--- src/services/thing-api-service.js
+++ src/services/thing-api-service.js
@@ -1,57 +1,44 @@
-import config from '../config'
+import config from '../config';
+import TokenService from './token-service';
 
 const ThingApiService = {
   getThings() {
     return fetch(`${config.API_ENDPOINT}/things`, {
-      headers: {
-      },
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
+      headers: {}
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
   },
   getThing(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}`, {
-      headers: {
-      },
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
+      headers: { Authorization: `Basic ${TokenService.getAuthToken()}` }
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
   },
   getThingReviews(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
-      headers: {
-      },
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
+      headers: { Authorization: `Basic ${TokenService.getAuthToken()}` }
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
   },
   postReview(thingId, text, rating) {
     return fetch(`${config.API_ENDPOINT}/reviews`, {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
+        Authorization: `Basic ${TokenService.getAuthToken()}`
       },
       body: JSON.stringify({
         thing_id: thingId,
         rating,
-        text,
-      }),
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
+        text
+      })
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
   }
-}
+};
 
-export default ThingApiService
+export default ThingApiService;
